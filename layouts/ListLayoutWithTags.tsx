efa3cd0384bc735fdb6e6fed1d6789ad110e53d8
UniClown <clown139880@gmail.com>
2025-12-15 15:14:48 +0800

    fix: update tag mapping syntax for consistency and readability
    chore: upgrade contentlayer2 and next-contentlayer2 to version 0.5.8
    
    chore: update pnpm-lock.yaml to reflect contentlayer2 version changes
    
    fix: change import assertion syntax in RSS script for compatibility

--- layouts/ListLayoutWithTags.tsx
+++ layouts/ListLayoutWithTags.tsx
@@ -145,7 +145,9 @@ export default function ListLayoutWithTags({
                             </h2>
                           )}
                           <div className="flex flex-wrap">
-                            {tags?.map((tag) => <Tag key={tag} text={tag} />)}
+                            {tags?.map((tag) => (
+                              <Tag key={tag} text={tag} />
+                            ))}
                           </div>
                         </div>
                         <div className="prose max-w-none whitespace-break-spaces text-gray-500 dark:text-gray-400">
